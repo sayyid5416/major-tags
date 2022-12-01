0864881aaf3ba032315713850ca43691bedc42c7
Sayyid <78584556+sayyid5416@users.noreply.github.com>
2022-12-01 23:18:45 +0530

    Update script.py
    - Proper methods of setting outputs and annotations are used.

--- src/script.py
+++ src/script.py
@@ -1,29 +1,50 @@
 import os
+from typing import Literal
 
 
-def env(name:str):
-    return os.environ.get(name, '')
+
+
+def env(name:str, _def=''):
+    return os.environ.get(name, _def)
+
+
+def set_output(key:str, value:str):
+    with open(env('GITHUB_OUTPUT'), 'a') as f:
+        f.write(f'{key}={value}')
+
+
+def set_annotation(
+    message:str,
+    title:str=None,
+    _type:Literal['debug', 'notice', 'warning', 'error']='notice',
+):
+    title = f' title={title}' if title else ''
+    print(f'::{_type}{title}::{message}')
+    if _type == 'error':
+        exit(1)
+
+
 
 
 # Collect data
 refName = env('GITHUB_REF_NAME')                                                    # [tag-version, branch-name, PR name, .....]
 refType = env('GITHUB_REF_TYPE')                                                    # [tag, branch, PR, ....]
 
+
 # Checks
 errorTitle, errorMsg = '', ''
 if refType != 'tag': 
-    errorTitle = 'Unsupported Type'
-    errorMsg = f'\\"{refType}\\" type is not supported. Run for tags only.'
+    set_annotation(
+        f'"{refType}" type is not supported. Run for tags only.',
+        'Unsupported Type',
+        'error'
+    )
 
-# Show error
-if errorTitle: errorTitle = f' title={errorTitle}'
-if errorMsg:
-    os.system(f'echo "::error{errorTitle}::{errorMsg}"')
-    exit(1)
 
 # Parsing
 majorTag = refName.split('.', 1)[0]                                                 # Tag name like v1, v2 etc
 
+
 # Set output
-os.system(f'echo "REF_NAME={refName}" >> $GITHUB_OUTPUT')
-os.system(f'echo "MAJOR_VER={majorTag}" >> $GITHUB_OUTPUT')
+set_output('REF_NAME', refName)
+set_output('MAJOR_VER', majorTag)
